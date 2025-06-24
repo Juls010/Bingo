24147741477b275ee1eba5849bedfd83c8cdef6f
Juls00 <juliagimena00@alumnos.cei.es>
2025-06-24 10:56:50 +0200

    new main

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,16 +5,13 @@ public class Main {
     public static void main(String[] args) {
         Drum drum = new Drum();
         
-
-        for (int i = 0; i < 95; i++) { // Intentamos sacar más de 90 bolas
-            Integer ball = drum.drawBall();
-            if (ball == null) {
-                System.out.println("All balls have been drawn. The game is over!");
-                break;
-            } else {
-                System.out.println("New ball: " + ball);
-            }
+        while(drum.hasBallsLeft()) {
+        	Integer ball = drum.drawBall();
+        	System.out.println("New ball: " + ball);
         }
+        System.out.println("Alls balls have been drawn. The game is over!");
+        
+        
     }
 }
 
